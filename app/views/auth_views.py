9e64d757ff3ac9a7f112f995efd5975dc7692fd1
wesolyigor <wesolowski.igor@gmail.com>
2020-12-12 23:08:49 +0100

    rework and add log out

--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, flash, request, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
 from werkzeug.utils import redirect
 
 from app import db
@@ -16,10 +16,9 @@ def login():
         user = User.get_by_username(form.username.data)
         if user is not None and user.check_password(form.password.data):
             login_user(user)
+            flash(f'You are logged in!')
             return redirect(request.args.get('next') or url_for('main.home'))
     return render_template('login.html', form=form)
-
-
 
 
 @bp_auth.route('/signup', methods=['GET', 'POST'])
@@ -31,8 +30,13 @@ def signup():
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('auth.signup'))
+        flash(f'Hello {user.username}! Please login')
+        return redirect(url_for('auth.login'))
     return render_template('signup.html', form=form)
 
 
-
+@bp_auth.route('/logout')
+def logout():
+    logout_user()
+    flash(f'Logged out!')
+    return redirect(url_for('main.home'))
